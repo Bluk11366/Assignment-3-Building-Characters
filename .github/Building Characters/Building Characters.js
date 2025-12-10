8a1119f48b13116f6ec321d6319efd24d260269c
Bluk1136 <bluk1136@gmail.com>
2025-12-09 22:09:33 -0500

    2.75

--- .github/Building Characters/Building Characters.js
+++ .github/Building Characters/Building Characters.js
@@ -1,15 +1,6 @@
-var canvas;
-var w = 400;
-var h = 400;
-
-setUpCanvas();
-
-function setUpCanvas(){
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
-    canvas.width = w;
-    canvas.height = h;
-    canvas.style.border = "4px solid purple";
+function setUpCanvas()
+{
+    createCanvas(400, 400);
 }
 
 
