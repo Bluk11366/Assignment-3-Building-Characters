8fce7c31c9bcd799137901d121bf66a3a3a299a3
Bluk1136 <bluk1136@gmail.com>
2025-12-12 06:55:51 -0500

    Update Building Characters.js

--- .github/Building Characters/Building Characters.js
+++ .github/Building Characters/Building Characters.js
@@ -17,5 +17,5 @@ function draw(){
     let currentTime = int(millis()/1000);
     textSize(24);
     fill(0, 75, 300);
-    text("Time:" + currentTime, 30, 380);
+    text("Time:" + currentTime, 30, 500);
 }
