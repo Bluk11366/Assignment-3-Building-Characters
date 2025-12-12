09f50ae6f4774ca2d2c3ceea02bc3045de3d4397
Bluk1136 <bluk1136@gmail.com>
2025-12-12 06:18:18 -0500

    Update Building Characters.js

--- .github/Building Characters/Building Characters.js
+++ .github/Building Characters/Building Characters.js
@@ -1,46 +1,57 @@
-var canvas;
-var Canvas;
-var w = 400;
-var h = 400;
-var windowWidth = windowWidth;
-var windowHeight = windowHeight;
+// var canvas;
+// var Canvas;
+// var w = 400;
+// var h = 400;
+// var windowWidth = windowWidth;
+// var windowHeight = windowHeight;
 
-let sprite1;
+// let sprite1;
 
-setUpCanvas();
-setup();
+// setUpCanvas();
+// setup();
 
-function preload(){
-    anim = loadAnimation("Images\PsVita1.png", ".github\Images\PsVita2.png", ".github\Images\PsVita3.png");
+// function preload(){
+//     anim = loadAnimation("Images\PsVita1.png", ".github\Images\PsVita2.png", ".github\Images\PsVita3.png");
 
-}
+// }
 
-function setUpCanvas(){
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
-    canvas.width = w;
-    canvas.height = h;
-    canvas.style.border = "4px solid purple";
-}
+// function setUpCanvas(){
+//     canvas = document.getElementById("canvas");
+//     ctx = canvas.getContext("2d");
+//     canvas.width = w;
+//     canvas.height = h;
+//     canvas.style.border = "4px solid purple";
+// }
+
+// function setup(){
+//     new Canvas(windowWidth, windowHeight);
+//     sprite1 = createSprite(windowWidth/2, windowHeight/2, 100, 100);
+//     sprite1.addAnimation("anim", anim);
+// }
+
+// function draw(){
+//     background(100);
+
+// }
+
+// function calculateHealth() {
+//   health -= 5;
+//   health = constrain(health, 0, 100);
+// }
+
+
+
+// function update(){
+//     clear();
+// }
 
 function setup(){
-    new Canvas(windowWidth, windowHeight);
-    sprite1 = createSprite(windowWidth/2, windowHeight/2, 100, 100);
-    sprite1.addAnimation("anim", anim);
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
-    background(100);
-
-}
-
-function calculateHealth() {
-  health -= 5;
-  health = constrain(health, 0, 100);
-}
-
-
-
-function update(){
-    clear();
+    background("black");
+    strokeWeight(40);
+    stroke("orange");
+    point(mouseX, mouseY);
 }
