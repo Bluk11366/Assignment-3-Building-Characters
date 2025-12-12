5746d965acdda5c21d54f5ea62f8f2a54d286a4a
Bluk1136 <bluk1136@gmail.com>
2025-12-12 11:03:06 -0500

    Update Building Characters.js

--- .github/Building Characters/Building Characters.js
+++ .github/Building Characters/Building Characters.js
@@ -1,15 +1,14 @@
-function preload(){
-    anim = loadImage('PsVita.gif');
-}
+let vita;
 
-function calculateHealth() {
-  health -= 5;
-  health = constrain(health, 0, 100);
+function preload(){
+    vitaAnim = loadImage('PsVita.gif');
 }
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
     frameRate(30);
+    vita = createSprite(windowWidth/2, windowHeight/2, 100, 100);
+    vita.addAnimation("vita", vitaAnim)
 }
 
 function draw(){
@@ -18,4 +17,9 @@ function draw(){
     textSize(24);
     fill(0, 75, 300);
     text("Time:" + currentTime, 30, windowHeight-30);
+}
+
+function calculateHealth() {
+  health -= 5;
+  health = constrain(health, 0, 100);
 }
